cfe57c48cc7a7f34b2ae751d993d63a0e2788147
Varada-eswar <yashwantvarada@gmail.com>
2026-04-25 00:02:26 +0530

    chore: Remove temporary helper files

--- check_html.py
+++
@@ -1,4 +0,0 @@
-﻿content = open('index_correct.html','r',encoding='utf-8').read()
-open_count = content.count('<div')
-close_count = content.count('</div>')
-print(f'<div count: {open_count}, </div> count: {close_count}, Balanced: {open_count==close_count}')

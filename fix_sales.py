7fedcc45733fa57defc56a2806a92d0fed4f0e34
Varada-eswar <yashwantvarada@gmail.com>
2026-04-25 00:02:13 +0530

    fix: Balance HTML div tags and restore + stat; clean working tree

---
+++ fix_sales.py
@@ -0,0 +1,16 @@
+﻿import re
+
+with open('index.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Build the replacement string without special chars
+dollar_sign = chr(36)
+old_pattern = '<div class="stat-number">' + '+' + '</div>'
+replacement = '<div class="stat-number">' + dollar_sign + '5B+' + '</div>'
+
+content = content.replace(old_pattern, replacement)
+
+with open('index.html', 'w', encoding='utf-8') as f:
+    f.write(content)
+
+print('Fixed Annual Sales stat')
---
+++ fix_stat.py
@@ -0,0 +1,12 @@
+﻿import re
+
+with open('index.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Fix the missing + 
+content = re.sub(r'<div class="stat-number">\+</div>', '<div class="stat-number">+</div>', content)
+
+with open('index.html', 'w', encoding='utf-8') as f:
+    f.write(content)
+
+print('Fixed + stat')
